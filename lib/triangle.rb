ddc2982c46dfa0266b96ca583b40b19f57efb19f
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-05-01 10:46:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,15 @@
 class Triangle
   # write code here
+  
+  attr_reader :right, :left, :bottom
+  
+  def initialize(right, left, bottom)
+    @right = right
+    @left = left
+    @bottom = bottom
+  end
+  
+  def type
+    
+  end
 end
